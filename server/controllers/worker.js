fa7b4da852982b508bae37850007ad808ed0639a
Hassan Jouda <hassan.jd.2006@gmail.com>
2022-06-26 21:31:33 +0300

    .

--- server/controllers/worker.js
+++ server/controllers/worker.js
@@ -1,12 +1,12 @@
 import worker from "../models/worker.js";
 
-export const getWorker = (req, res) => {
-  worker
-    .find()
-    .then((worker) => {
-      return res.json(worker);
-    })
-    .catch((err) => res.status(400).json(err.message));
+export const getWorker = async (req, res) => {
+  try {
+    const data = await worker.find();
+    res.json(data);
+  } catch (error) {
+    res.send(error.message);
+  }
 };
 
 export const addWorker = (req, res) => {
